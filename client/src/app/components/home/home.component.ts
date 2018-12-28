9c292f7fa01b69f8a066a02d39b4ea71dfb6e3ef
manuelgdc <manuelgdc@protonmail.com>
2018-12-28 21:02:34 +0100

    homeComponent

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DataApiService } from 'src/app/services/data-api.service';
-
+import { BookInterface} from '../../models/book-interfaces';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  private books: BookInterface;
 
   constructor(private dataApi: DataApiService ) { }
 
@@ -16,7 +18,7 @@ export class HomeComponent implements OnInit {
 
   getListBook(){
     this.dataApi.getAllBooks()
-    .subscribe(books => console.log(books));
+    .subscribe((books: BookInterface) => (this.books = books));
   }
 
   
